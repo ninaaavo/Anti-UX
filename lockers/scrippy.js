13710b83de4fac794f7e2462d931543724f24371
ninaaavo <nbvo@umass.edu>
2026-03-07 11:28:55 -0500

    add overlau

--- lockers/scrippy.js
+++ lockers/scrippy.js
@@ -1,17 +1,29 @@
 const lockers = Array.from(document.getElementsByClassName("locker"));
 const overlay = document.getElementById('overlay');
-const zoomedImg = document.getElementById('zoomedImg');
+const zoomedLocker = document.getElementById('zoomedLocker');
 console.log("yoooo");
-console.log("all locekrs", lockers[0]);
+console.log("all lockers", lockers[0]);
 
 console.log(Array.isArray(lockers));
 lockers.forEach((locker, ind) => {
-    locker.addEventListener('click', () => {
-    //   zoomedImg.src = locker.src;
-    //   zoomedImg.alt = locker.alt;
-    //   overlay.classList.remove('hidden');
-    alert("clicked!! " + (ind + 1))
+    locker.addEventListener('click', (event) => {
+      if (overlay.classList.contains('hidden')){
+        event.stopPropagation()
+        zoomedLocker.src = locker.src;
+        overlay.classList.remove('hidden');
+        console.log("clicked!! " + (ind + 1))
+      }
     });
-  });
+});
+
+// when click outside of overlay, exit
+document.addEventListener('click', (event) => {
+  console.log("class lst" ,overlay.classList)
+  if(!overlay.contains(event.target) && !overlay.classList.contains('hidden')){
+    console.log("clicked outside");
+    overlay.classList.add('hidden');
+    console.log("overlay list", overlay.classList)
+  }
+})
 
   
